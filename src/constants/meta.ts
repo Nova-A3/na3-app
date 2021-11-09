1086bffb635150dc6480b2f95ae3baa4acaa0c2e
santosmarco <eu@marco.rio.br>
2021-11-08 22:46:00 -0300

    v1.0.6

--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -1,12 +1,3 @@
-/**
- * ==> CAUTION <==
- *
- * This file is read and updated automatically by the _build_ script.
- *
- * DO NOT EDIT IT MANUALLY.
- *
- *
- *
- */
+/* DO **NOT** EDIT THIS FILE. */
 
-export const APP_VERSION = "1.0.5";
+export const APP_VERSION = "1.0.6";
