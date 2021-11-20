5a556bc6af26406c60037961dd16cc09350f7394
santosmarco <eu@marco.rio.br>
2021-11-20 13:12:13 -0300

    v1.0.36

--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -1,4 +1,4 @@
 /* DO **NOT** EDIT THIS FILE. */
 
-export const APP_VERSION = "1.0.36";
-export const APP_VERSION_TIMESTAMP = "2021-11-20T12:04:27-03:00";
+export const APP_VERSION = "1.0.37";
+export const APP_VERSION_TIMESTAMP = "2021-11-20T13:07:21-03:00";
